d25826b03d083a70b5c83829719c0f43723e13fe
basirat10 <basiratamusat10@gmail.com>
2024-01-03 14:55:25 +0000

    java script 3

---
+++ index.js
@@ -0,0 +1,12 @@
+let welcome = document.getElementById("welcome=el")
+
+let name = "per Harald Borgen"
+let greeting = "welcome back "
+
+welcomeEl.innerText = greeting + name
+
+//Add an emoji to the end! 👋
+//WRITE YOUR CODE BELOW HERE
+//HINT: count = count + 1
+
+welcomeEl.innerText += "👋"
